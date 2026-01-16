d5f583a94569f986bb7a110f11b18f34d62008dd
KokosAnanas <kurochkinrv@list.ru>
2026-01-17 00:38:34 +0300

    feat(Nx ws): init Nx, add web & api

---
+++ apps/web/postcss.config.js
@@ -0,0 +1,8 @@
+// PostCSS конфигурация для Tailwind CSS
+// Документация: https://tailwindcss.com/docs/installation/using-postcss
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
